da4fce74ff48e78e5b4528eecde0639314b41597
Anurag Prasad <anuragp0010@gmail.com>
2021-09-27 03:05:50 +0530

    Removed JDBC part

--- src/main/java/com/mindtree/customer/customer/service/CustomerService.java
+++ src/main/java/com/mindtree/customer/customer/service/CustomerService.java
@@ -1,5 +1,6 @@
 package com.mindtree.customer.customer.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,21 +11,23 @@ import com.mindtree.customer.customer.user.Customer;
 
 
 @Repository
-public class CustomerService implements CustomerServiceImpl {
+public class CustomerService /*implements CustomerServiceImpl*/ {
 	
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	
-//	private static List<Customer> customers = new ArrayList<>();
+	private static List<Customer> customers = new ArrayList<>();
 	
 //	private static int usersCount = 0;
 	
-//	static
-//	{
-//		customers.add(new Customer(1,"Anurag","Kadma"));
-//		customers.add(new Customer(2,"Ankur","Adityapur"));
-//		customers.add(new Customer(3,"Neelesh","Bangalore"));
-//	}
+	static
+	{
+		customers.add(new Customer(1,"Anurag","Kadma"));
+		customers.add(new Customer(2,"Ankur","Adityapur"));
+		customers.add(new Customer(3,"Neelesh","Bangalore"));
+	}
+	
+	
 	
 //	@Transactional
 //	public Customer save(Customer customer)
@@ -36,51 +39,51 @@ public class CustomerService implements CustomerServiceImpl {
 //		customers.add(customer);
 //		return customer;
 //	}
-
-	@Override
+//
+//	@Override
 	public List<Customer> getAllCustomer() {
 		// TODO Auto-generated method stub
-//		return customers;
-		return jdbcTemplate.query("SELECT * FROM customer_details.customer_table", new BeanPropertyRowMapper<Customer>(Customer.class));
+		return customers;
+//		return jdbcTemplate.query("SELECT * FROM customer_details.customer_table", new BeanPropertyRowMapper<Customer>(Customer.class));
 	}
-
-	@Override
-	public Customer getCustomer(int id) {
+//
+//	@Override
+//	public Customer getCustomer(int id) {
 //		for (Customer customer : customers) {
 //			
 //			if(customer.getId() == id)
 //				return customer;
 //		}
 //		return null;
-		
-		return jdbcTemplate.queryForObject("SELECT * FROM customer_details.customer_table c WHERE c.id=?", new BeanPropertyRowMapper<Customer>(Customer.class), id);
-	}
-
-	@Override
-	public int addCustomer(Customer customer) {
+//		
+//		return jdbcTemplate.queryForObject("SELECT * FROM customer_details.customer_table c WHERE c.id=?", new BeanPropertyRowMapper<Customer>(Customer.class), id);
+//	}
+//
+//	@Override
+//	public int addCustomer(Customer customer) {
 //		if(customer.getId() == 0)
 //		{
 //			customer.setId(usersCount);
 //		}
 //		customers.add(customer);
 //		return customer;
-		
-		return jdbcTemplate.update("INSERT INTO customer_details.customer_table(name, address) VALUES (?, ?)", new Object[] {customer.getName(), customer.getAddress()});
-	}
-
-	@Override
-	public int deleteCustomer(int id) {
-		// TODO Auto-generated method stub
-		return jdbcTemplate.update("DELETE FROM customer_details.customer_table c WHERE c.id=?", id);
+//		
+//		return jdbcTemplate.update("INSERT INTO customer_details.customer_table(name, address) VALUES (?, ?)", new Object[] {customer.getName(), customer.getAddress()});
+//	}
+//
+//	@Override
+//	public int deleteCustomer(int id) {
+//		// TODO Auto-generated method stub
+//		return jdbcTemplate.update("DELETE FROM customer_details.customer_table c WHERE c.id=?", id);
 //		return null;
-	}
-
-	@Override
-	public int updateCustomer(Customer customer, int id) {
-		// TODO Auto-generated method stub
-		return jdbcTemplate.update("UPDATE customer_details.customer_table c SET c.name = ?, c.address = ? WHERE c.id = ?", new Object[] {customer.getName(), customer.getAddress(), id});
+//	}
+//
+//	@Override
+//	public int updateCustomer(Customer customer, int id) {
+//		// TODO Auto-generated method stub
+//		return jdbcTemplate.update("UPDATE customer_details.customer_table c SET c.name = ?, c.address = ? WHERE c.id = ?", new Object[] {customer.getName(), customer.getAddress(), id});
 //		return null;
-	}
+//	}
 
 	
 
